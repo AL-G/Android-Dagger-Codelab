b896f47dadbe41fe842781593b87236dd10d04b8
alan <alan.godden@gmail.com>
2020-10-21 20:30:44 +0100

    Initial commit

---
+++ app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.dagger.di
+
+import android.content.Context
+import com.example.android.dagger.main.MainActivity
+import com.example.android.dagger.registration.RegistrationActivity
+import com.example.android.dagger.registration.RegistrationComponent
+import com.example.android.dagger.registration.enterdetails.EnterDetailsFragment
+import com.example.android.dagger.registration.termsandconditions.TermsAndConditionsFragment
+import com.example.android.dagger.settings.SettingsActivity
+import com.example.android.dagger.user.UserComponent
+import com.example.android.dagger.user.UserManager
+import dagger.BindsInstance
+import dagger.Component
+import javax.inject.Singleton
+
+// Scope annotation that the AppComponent uses
+// Classes annotated with @Singleton will have a unique instance in this Component
+@Singleton
+// Definition of a Dagger component that adds info from the StorageModule to the graph
+@Component(modules = [StorageModule::class, AppSubcomponents::class])
+interface AppComponent {
+
+    // Factory to create instances of the AppComponent
+    @Component.Factory
+    interface Factory {
+        // With @BindsInstance, the Context passed in will be available in the graph
+        fun create(@BindsInstance context: Context): AppComponent
+    }
+
+    // 2) Expose UserManager so that MainActivity and SettingsActivity
+    // can access a particular instance of UserComponent
+    fun userManager(): UserManager
+
+    // Expose RegistrationComponent factory from the graph
+    fun registrationComponent(): RegistrationComponent.Factory
+    // Expose LoginComponent factory from the graph
+    fun loginComponent(): LoginComponent.Factory
+
+}
